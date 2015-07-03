18ad576c69af1c043ec1a32d3aefed8fe39143e2
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-07-03 23:45:07 +0300

    scenario validators

--- src/scenario.js
+++ src/scenario.js
@@ -7,7 +7,7 @@ var _ = require('underscore');
 /start
  /setup
  .- /audio
- .   /ambition : set ambition audio level
+ .   /ambient : set ambient audio level
  .   /music : set music level
  .- /video
  .   /resolution : large resolution select, use list:
@@ -44,6 +44,157 @@ var _ = require('underscore');
  /back
 */
 
+
+/**
+ * Simple Validator. For internal usage,
+ *
+ * Most of validators allow you use function or function name in string.
+ * Allowed function names passed as api in constructor. Unknown fun name is reason
+ * for throwing error.
+ *
+ * @example
+ * let v2 = new Validator(api),
+ *     boolean = v2.boolean.bind(v2);
+ * boolean('propname', true); // ok
+ * boolean('propname', "<% someApiCall %>"); // ok
+ * boolean('propname', () => true ); // ok
+ * boolean('propname', "XYZ<% someApiCall %>"); // fail because fun str may be "<% .. %>"
+ *
+ * @param {object} api - hash of known methods that may be used by "<% XYZ %>"
+ */
+function Validator(api={}) {
+  this.api = api;
+}
+
+Validator.prototype = {
+  isFunction(value) {
+    if (_.isFunction(value)) {
+      return true;
+    } else if (_.isString(value)) {
+      let found = value.match(/^<%\s*(\w+)\s*%>$/);
+      if (! found)
+        return false;
+      let function_name = found[1];
+      if ( function_name &&
+           _.has(this.api, function_name)) {
+        return true;
+      } else {
+        throw Error (`Function "${function_name}" that used in "<% %>" is undefined`);
+      }
+    }
+    return false;
+  },
+
+  boolean(key_name, value) {
+    if (! _.isBoolean(value) &&
+        ! this.isFunction(value))
+      throw Error(`"${key_name}" must be boolean or function`);
+  },
+
+  number(key_name, value) {
+    if ( ! _.isNumber(value) &&
+         ! this.isFunction(value))
+      throw Error(`"${key_name}" must be number or function`);
+  },
+
+  string(key_name, value) {
+    if (_.isFunction(value))
+      return true;
+
+    if (! _.isString(value))
+      throw Error(`"${key_name}" must be string or function`);
+
+    let re = /<%\s*(\w+)\s*%>/g,
+        match;
+
+    while ((match = re.exec(value))) {
+      let function_name = match[1];
+      if (! _.has(this.api, function_name))
+        throw Error (`Function "${function_name}" that used in "<% %>" is undefined`);
+    }
+    return true;
+  },
+
+  oneOf(validators, key_name, value) {
+    if (! validators || ! _.isArray(validators)) {
+      throw Error(`[oneOf] validators must be not empty array`);
+    }
+
+    let valid = false,
+        errors = [];
+    _.each(validators, (validator) => {
+      try {
+        validator(key_name, value);
+        valid = true;
+      } catch (err) {
+        errors.push(err);
+      }
+    });
+    if (! valid) {
+      let error_msg = errors.join('\n  ');
+      throw Error(`None of validators returned success for property ${key_name}:\n  ${error_msg}`);
+    }
+
+  },
+
+  array(arrayOf, key_name, value) {
+    if (! arrayOf || ! _.isArray(arrayOf)) {
+      throw Error(`[array] arrayOf must be not empty array`);
+    }
+
+    if (! _.isArray(value) &&
+        ! this.isFunction(value))
+      throw Error(`"${key_name}" must be array or function that return array`);
+
+    if (_.isArray(value)) {
+      _.each(value, (item, index) => {
+        let key_name_indexed = `${key_name}[${index}]`;
+        this.oneOf(arrayOf, key_name_indexed, item);
+      });
+    }
+  },
+
+  object(schema, required_keys, key_name, value) {
+    if (_.isFunction(schema)) {
+      schema = schema();
+    }
+
+    if (! _.isObject(schema) || _.isArray(schema)) {
+      throw Error(`schema is empty for "${key_name}"`);
+    }
+
+    if (! _.isObject(value) || _.isArray(value)) {
+      throw Error(`"${key_name}" must be hash object`);
+    }
+
+    _.each(required_keys, (key) => {
+      if (! _.has(value, key)) {
+        throw Error(`"${key_name}" expect to have key "${key}"`);
+        }
+    });
+
+    //now validate all properties of schema
+    _.each(schema, function(sub_validator, schema_key_name) {
+      // check only properties if they exists, we heck  required before
+      if (_.has(value, schema_key_name)) {
+        let new_key_name = key_name + '.' + schema_key_name;
+        sub_validator(new_key_name, value[schema_key_name]);
+      }
+    });
+
+    // throw if here is unknown keys in "value" object
+    let schema_keys  = Object.keys(schema),
+        value_keys   = Object.keys(value),
+        unknown_keys = _.difference(value_keys, schema_keys);
+
+    if(! _.isEmpty(unknown_keys)) {
+      let list_of_kes = unknown_keys.join('", "');
+      throw Error(`"${key_name}" object use unknown keys: ["${list_of_kes}"]. Maybe you mistype?`);
+    }
+  },
+
+};
+
 function Scenario(scenario) {
   this._scenario = this.validate(scenario);
 
@@ -75,35 +226,16 @@ Scenario.prototype = {
       if (! _.has(schema, prop)) {
         throw Error(`Scenario invalid: Unexpected property "${prop}". Path: ${path}`);
         } else {
-          let valid = false,
-              prop_value = scenario[prop];
-          for (let type of schema[prop]) {
-            if (type === 'string' && _.isString(prop_value)) {
-              valid = true; break;
-            }
-            else if (type === 'array' && _.isArray(prop_value)) {
-              valid = true; break;
-            }
-            else if (type === 'function' && _.isFunction(prop_value)) {
-              valid = true; break;
-            }
-            else if (type === 'boolean' && _.isBoolean(prop_value)) {
-              valid = true; break;
-            }
-            else if (type === 'number' && _.isNumber(prop_value)) {
-              valid = true; break;
-            }
-          }
-          if (! valid)
-            throw Error(`Scenario invalid: Property "${prop}" must be one of ${ schema[prop].join(', ') }. Path: ${path}`);
+          let prop_value = scenario[prop];
+          this._validateType(prop_value, schema[prop]);
       }
     }
 
     return sc;
-  }
+  },
 };
 
-export {Scenario};
+export {Scenario, Validator};
 
 // var scenario = {
 //   // Type of action to broadcast: Choose one, depending on what the user is about to receive
@@ -147,3 +279,109 @@ export {Scenario};
 //   }
 
 // };
+
+// V = {
+//   boolean: function(key, value) {
+
+//   }
+// };
+
+// var scenario = {
+//   "Typing": true,
+//   "Before": "<% beforeRoot %>",
+//   "Commands": {
+//     "/start": {
+//       Scenario: {
+//         Commands: {
+//           "/audio": {
+//             "Text": "Audio settings, use /ambient or /music commands",
+//             "Menu": [
+//               "Ambient",
+//               "Music",
+//               ["Back", "Cancel"]
+//             ],
+//             "Scenario": {
+//               Menu: [
+//                 "0",
+//                 "20",
+//                 "50",
+//                 "75",
+//                 "100",
+//                 ["Back", "Cancel"]
+//               ],
+//               "/ambient | ambient": {
+//                 text: "Tell me, ambient level, number 0..100, current level is <% getAmbientLvl %>.",
+//                 "Scenario": {
+//                   "/back || back": {
+//                     "Goto": "<% _back %>"
+//                   },
+//                   "/cancel || cancel": {
+//                     "Goto": "/"
+//                   },
+//                   "default": {
+//                     "Action": "<% setAmbientMaybe %>",
+//                     "goto": "<% _back %>"
+//                   }
+//                 },
+//               },
+//               "/music || music" : {
+//                 Menu: [
+//                   "0",
+//                   "20",
+//                   "50",
+//                   "75",
+//                   "100",
+//                   ["Back", "Cancel"]
+//                 ],
+//                 text: "Tell me, music level, number 0..100, current level is <% getMusicLvl%>",
+//                 Scenario: {
+//                   "/back": {
+//                     "Goto": "<% _back %>"
+//                   },
+//                   "/cancel": {
+//                     "Goto": "/"
+//                   },
+//                   "default": {
+//                     Action: "<% setMusicMaybe %>",
+//                     "goto": "<% _back %>"
+//                   }
+//                 }
+//               }
+//             }
+//           },
+//           "/video": {
+
+//           },
+//           "cancel": {
+//           }
+//         }
+//       }
+//     },
+//     default: "<% defaultRoot %>"
+//   }
+// }
+
+// var minimal = {
+//   typing: V.boolean,
+//   upload_photo: V.boolean,
+//   record_video: V.boolean,
+//   upload_video: true,
+//   record_audio: true,
+//   upload_audio: true,
+//   upload_document: true,
+//   find_location: true,
+
+//   Scenario: {
+//     Text: "",
+//     Menu: [ // array of (string || array of string)
+
+//     ],
+//     Before: "<>",                  // run when enter to this scenario
+//     Action: "<>",                  //
+//     Goto: "/",
+//     Commands: {
+//       "command name": SCENARIO
+//     }
+//   }
+
+// },
--- test/unit/scenatio.js
+++ test/unit/scenatio.js
@@ -1,6 +1,341 @@
-import {Scenario} from '../../src/scenario.js';
+'use strict';
+
+import {Scenario, Validator} from '../../src/scenario.js';
 
 chai.should();
+
+describe("validators function:", function() {
+  it('"boolean" expect to be boolean, string "<% foo %>" or function', function() {
+    let api = {foo: () => {}},
+        v = new Validator(api);
+
+    expect(v.boolean).bind(v).called('foo', true).not.to.throw();
+    expect(v.boolean).bind(v).called('foo', false).not.to.throw();
+
+    expect(v.boolean).bind(v).called('foo', 123).to.throw('"foo" must be boolean');
+
+    expect(v.boolean).bind(v).called('foo', () => {}).not.to.throw();
+
+    expect(v.boolean).bind(v).called('foo', "").to.throw('"foo" must be boolean');
+    expect(v.boolean).bind(v).called('foo', "foo").to.throw();
+
+    expect(v.boolean).bind(v).called('foo', "<% foo %>").to.not.throw();
+    expect(v.boolean).bind(v).called('foo', "<% foo %>baz").to.throw();
+    expect(v.boolean).bind(v).called('foo', "<% bar %>").to.throw('undef');
+  });
+
+  it('"number" expect value number. Function or function name"<% foo %>" must work too', function() {
+    let api = {foo: () => {}},
+        v = new Validator(api);
+
+    expect(v.number).bind(v).called('foo1', true).to.throw('"foo1" must be number');
+    expect(v.number).bind(v).called('foo1', false).to.throw();
+    expect(v.number).bind(v).called('foo2', []).to.throw();
+
+    expect(v.number).bind(v).called('foo2', 123).not.to.throw();
+
+    expect(v.number).bind(v).called('foo3', () => {}).not.to.throw();
+
+    expect(v.number).bind(v).called('foo4', "<% foo %>").to.not.throw();
+    expect(v.number).bind(v).called('foo5', "<% foo %>baz").to.throw();
+    expect(v.number).bind(v).called('foo7', "<% bar %>").to.throw('undef');
+
+    expect(v.number).bind(v).called('foo8', "").to.throw();
+    expect(v.number).bind(v).called('foo9', "123").to.throw();
+    expect(v.number).bind(v).called('foo10', "abc").to.throw('number');
+  });
+
+  it('"string" expect value string. Function or function name"<% foo %>" must work too', function() {
+    let api = {
+      foo: () => {},
+      baz: () => {}
+    },
+        v = new Validator(api);
+
+    expect(v.string).bind(v).called('foo1', true).to.throw();
+    expect(v.string).bind(v).called('foo2', false).to.throw();
+    expect(v.string).bind(v).called('foo3', []).to.throw();
+    expect(v.string).bind(v).called('foo4', {}).to.throw();
+
+    expect(v.string).bind(v).called('foo5', 123).to.throw('"foo5" must be string');
+
+    expect(v.string).bind(v).called('foo6', () => {}).not.to.throw();
+
+    expect(v.string).bind(v).called('foo7', "<% foo %>").to.not.throw();
+    expect(v.string).bind(v).called('foo8', "<% foo %>baz").to.not.throw();
+    expect(v.string).bind(v).called('foo9', "<% foo %> baz <% baz %>").to.not.throw();
+    expect(v.string).bind(v).called('foo10', "text <% foo %>baz").to.not.throw();
+    expect(v.string).bind(v).called('foo11', "<% bar %>").to.throw();
+    expect(v.string).bind(v).called('foo12', "<% foo %> baz <% bar %>").to.throw();
+
+    expect(v.string).bind(v).called('foo13', "baz <% bar %>").to.throw('undef');
+
+    expect(v.string).bind(v).called('foo14', "").to.not.throw();
+    expect(v.string).bind(v).called('foo15', "123").to.not.throw();
+    expect(v.string).bind(v).called('foo16', "abc").to.not.throw('string');
+  });
+
+
+  it('"oneOf" expect for at least one valid type in array of types', function() {
+    let api = {
+      foo: () => {},
+    },
+        v = new Validator(api),
+        string = v.string.bind(v),
+        number = v.number.bind(v);
+
+    expect(v.oneOf).bind(v).called([string], 'foo1', "abc").to.not.throw();
+    expect(v.oneOf).bind(v).called([string], 'foo2', "").to.not.throw();
+    expect(v.oneOf).bind(v).called([string], 'foo3', 123).to.throw('None of validators');
+
+    let oneOf_str_or_num = v.oneOf.bind(v, [string, number]);
+    expect(oneOf_str_or_num).called('foo4', 123).to.not.throw();
+    expect(oneOf_str_or_num).called('foo5', "<% foo %>").to.not.throw();
+    expect(oneOf_str_or_num).called('foo6', "<% foo %>").to.not.throw();
+    expect(oneOf_str_or_num).called('foo7', []).to.throw('None of validators');
+
+    expect(v.oneOf).bind(v).called(null, 'foo1', "abc").to.throw('must be not empty array');
+  });
+
+  it('"array" expect value array or function, function name"<% foo %>"', function() {
+    let api = {
+      foo: () => {},
+    },
+        v = new Validator(api),
+        array = v.array.bind(v),
+        string = v.string.bind(v),
+        number = v.number.bind(v);
+
+    expect(array).bind(v).called('foo1', []).to.throw('arrayOf must be not empty array');
+
+    expect(array).bind(v).called([ string ], 'foo1', false).to.throw('must be array');
+    expect(array).bind(v).called([ string ], 'foo2', []).to.not.throw('must be array');
+    expect(array).bind(v).called([ string ], 'foo3',
+                                 [ "abc", "def" ]).to.not.throw();
+
+    expect(array).bind(v).called([ number ], 'foo3',
+                                 "").to.throw();
+
+    expect(array).bind(v).called([ number ], 'foo3',
+                                 "abc").to.throw();
+
+    // throw, because  string only acceptable
+    expect(array).bind(v).called([ string ], 'foo4',
+                                 [ "abc", 123]).to.throw();
+
+    expect(array).bind(v).called([ string ], 'foo4',
+                                 [ "abc", {}]).to.throw();
+
+    expect(array).bind(v).called([ string, number ], 'foo5',
+                                 [ "abc", 123]).to.not.throw();
+
+    expect(array).bind(v).called([ string, number ], 'foo6',
+                                 [ "abc", () => {} ]).to.not.throw();
+
+    expect(array).bind(v).called([ string, number ], 'foo6',
+                                 "<% foo %>").to.not.throw();
+
+    expect(array).bind(v).called([ number ], 'foo6',
+                                 [ 123,
+                                   () => {},
+                                   "<% foo %>"
+                                 ]).to.not.throw();
+
+    expect(array).bind(v).called([ number ], 'foo6',
+                                 [ 123,
+                                   () => {},
+                                   "<% foo %>"
+                                 ]).to.not.throw();
+  });
+
+  it('"array" expect for support nested array of array of XXX', function() {
+    let api = {
+      foo: () => {},
+    },
+        v                            = new Validator(api),
+        array                        = v.array.bind(v),
+        string                       = v.string.bind(v),
+        number                       = v.number.bind(v),
+        array_of_str                 = array.bind(v, [string]),
+        array_of_array_of_str        = array.bind(v, [array_of_str]),
+        array_of_array_of_str_or_num = array.bind(v, [array.bind(v, [string, number])]),
+        array_of_str_or_array_of_str = v.array.bind(v, [string, array_of_str]);
+
+    expect(array_of_array_of_str).called('foo7', [ ["ddd" ] ]).to.not.throw();
+
+    expect(array_of_array_of_str).called('foo7',
+                                         [ ["yes", "no" ],
+                                           ["more", "done" ]
+                                         ]).to.not.throw();
+
+    expect(array_of_array_of_str).called('foo7',
+                                         [ ["yes", "no" ],
+                                           "cancel"
+                                         ]).to.throw('None of validators');
+
+    // array of array of strings or numbers
+    expect(array).bind(v).called([ array.bind(v, [string])  ], 'foo8',
+                                 [ ["ddd", 1234 ] ]).to.throw('must be string');
+
+    expect(array_of_array_of_str_or_num).called('foo9',
+                                                [ ["ddd", 1234 ] ]).to.not.throw();
+
+    expect(array_of_array_of_str_or_num).called('foo9',
+                                                [ ["<% foo %>", 1234 ] ]).to.not.throw();
+
+    // it may be function that we expect return array
+    expect(array_of_array_of_str_or_num).called('foo9',
+                                                [ "<% foo %>" ]).to.not.throw();
+
+    // more complex: array of (strings or array of strings)
+    expect(array_of_str_or_array_of_str).called('foo10',
+                                                [
+                                                  [ "yes", "no", ],
+                                                  [ "back"       ],
+                                                  "cancel",
+                                                ]).to.not.throw();
+  });
+
+  it('"object" expect value to be not array but object only', function() {
+    let api = {
+      foo: () => {},
+    },
+        v = new Validator(api),
+        string = v.string.bind(v),
+        number = v.number.bind(v),
+        schema = {
+          req_num: number,
+          str: string};
+
+    // empty schema
+    expect(v.object).bind(v).called(null, /* schema */
+                                    ['req_num'], /* required */
+                                    'foo1', /* property name*/
+                                    {} /* object */
+                                    ).to.throw('schema is empty for "foo1"');
+
+    // null object
+    expect(v.object).bind(v).called(schema, /* schema */
+                                    ['req_num'], /* required */
+                                    'foo2', /* property name*/
+                                    null /* object */
+                                    ).to.throw('"foo2" must be hash object');
+
+    // req_num field is required
+    expect(v.object).bind(v).called(schema, /* schema */
+                                    ['req_num'], /* required */
+                                    'foo1', /* property name*/
+                                    {} /* object */
+                                    ).to.throw('expect to have key "req_num"');
+
+    // good, have required and valid type
+    expect(v.object).bind(v).called(schema, /* schema */
+                                    ['req_num'], /* required */
+                                    'foo1', /* property name*/
+                                    {req_num: 123} /* object */
+                                    ).to.not.throw();
+    // get schema via callback
+    expect(v.object).bind(v).called(() => schema, /* function get schema */
+                                    ['req_num'], /* required */
+                                    'foo1', /* property name*/
+                                    {req_num: 123} /* object */
+                                    ).to.not.throw();
+
+    // invalid required
+    expect(v.object).bind(v).called(schema, /* schema */
+                                    ['req_num'], /* required */
+                                    'foo1', /* property name*/
+                                    {req_num: "abc"} /* object */
+                                    ).to.throw('number');
+    // another invalid type, str
+    expect(v.object).bind(v).called(schema, /* schema */
+                                    ['req_num'], /* required */
+                                    'foo1', /* property name*/
+                                    {
+                                      req_num: 123, /* object */
+                                      str: 124443 }
+                                    ).to.throw('string');
+
+    // unknown property of object
+    expect(v.object).bind(v).called(schema, /* schema */
+                                    ['req_num'], /* required */
+                                    'foo1', /* property name*/
+                                    {
+                                      fail_prop1: 1, /* object */
+                                      fail_prop2: 2,
+                                      req_num: 123 }
+                                    ).to.throw('unknown keys: ["fail_prop1", "fail_prop2"]');
+  });
+
+  it('expect nested object validate', function() {
+    let v = new Validator(),
+        object                       = v.object.bind(v),
+        boolean                      = v.boolean.bind(v),
+        number                       = v.number.bind(v),
+        string                       = v.string.bind(v),
+        array                        = v.array.bind(v),
+        array_of_str                 = array.bind(v, [string]),
+        array_of_str_or_array_of_str = v.array.bind(v, [string, array_of_str]);
+
+
+    // must be before scenario_schema created,
+    let scenario_schema = {};
+
+    let scenario_validator = v.object.bind(v,
+                                           () => scenario_schema,   // schema
+                                           []                       // required
+                                          );
+    scenario_schema = {
+      typing: boolean,
+      Menu: array_of_str_or_array_of_str,
+      Scenario: scenario_validator
+    };
+
+    // not nested
+    expect(scenario_validator, 'Not nested')
+      .called('foo1', /* property name*/
+              {       /* oject */
+                typing: false,
+              }
+             ).to.not.throw();
+
+    expect(scenario_validator, 'Nested Object')
+      .called('foo2', /* property name*/
+              {       /* oject */
+                typing: false,
+                Scenario: {
+                  typing: true
+                }
+              }
+             ).to.not.throw();
+
+    expect(scenario_validator, 'Failed validation')
+      .called('foo3', /* property name*/
+              {       /* oject */
+                typing: false,
+                Scenario: {
+                  typing: "abc" // <- fail here, boolean ecpected
+                }
+              }
+             ).to.throw('"foo3.Scenario.typing" must be boolean');
+
+    expect(scenario_validator, 'Double nester Scenarios')
+      .called('foo3', /* property name*/
+              {       /* oject */
+                typing: false,
+                Scenario: {
+                  Scenario: {
+                    Menu: [
+                      ['Start', 'End'],
+                      "fooo"
+                    ],
+                  },
+                  typing: false
+                }
+              }
+             ).to.not.throw();
+  });
+});
 
 describe('Scenario class', function(){
   // it('should fail when asserting false', function(){
@@ -18,104 +353,4 @@ describe('Scenario class', function(){
 
     Scenario.prototype.validate.restore();
   });
-});
-
-describe('Scenario validator', function() {
-  it('expect throw when passed not object', function(){
-    expect(Scenario.prototype.validate).called().to.throw();
-  });
-
-  // it('shoud pass when only one root item that MUST have "text" field', function(){
-  //   let scenario = {
-  //     text: "root menu"
-  //   };
-  //   expect(Scenario.prototype.validate).called(scenario).to.not.throw();
-  //   // not "text" field
-  //   expect(Scenario.prototype.validate).called({}).to.throw();
-  // });
-  it('expect throw when unknown property used', function() {
-    let scenario = {
-      foobar: "baz"
-    };
-    expect(Scenario.prototype.validate).called(scenario).to.throw();
-  });
-
-  it('expect pass when known only known property have', function() {
-    let scenario = { text: "baz" };
-    expect(Scenario.prototype.validate).called(scenario).to.not.throw();
-
-    // other known
-    scenario.typing = true;
-    expect(Scenario.prototype.validate).called(scenario).to.not.throw();
-
-    // add unknown
-    scenario.baz = true;
-    expect(Scenario.prototype.validate).called(scenario).to.throw();
-  });
-
-  it('expect valid "string" type of schema keys', function() {
-    let scenario_ok = { text: "baz" };
-    let schema = { text: ['string'] };
-    expect(Scenario.prototype.validate).called(scenario_ok, '/', schema)
-      .to.not.throw();
-
-    let scenario_invalide = { text: [1,2,3] };
-    expect(Scenario.prototype.validate).called(scenario_invalide, '/', schema)
-      .to.throw();
-  });
-
-  it('expect valid "boolean" type of schema keys', function() {
-    let schema = { text: ['boolean'] };
-
-    let scenario_ok = {
-      text: false
-    };
-    expect(Scenario.prototype.validate).called(scenario_ok, '/', schema)
-      .to.not.throw();
-
-    let scenario_invalide = {
-      text: [1,2,3]
-    };
-    expect(Scenario.prototype.validate).called(scenario_invalide, '/', schema)
-      .to.throw();
-  });
-
-  it('expect valid "array" type of schema keys', function() {
-    let schema = { text: ['array'] };
-
-    let scenario_ok = { text: [1,2,3] };
-    expect(Scenario.prototype.validate).called(scenario_ok, '/', schema)
-      .to.not.throw();
-
-    let scenario_invalide = { text: "foobar" };
-    expect(Scenario.prototype.validate).called(scenario_invalide, '/', schema)
-      .to.throw();
-  });
-
-  it('expect valid "function" type of schema keys', function() {
-    let schema = { text: ['function'] };
-
-    let scenario_ok = {
-      text: function() {}
-    };
-    expect(Scenario.prototype.validate).called(scenario_ok, '/', schema)
-      .to.not.throw();
-
-    let scenario_invalide = { text: 123 };
-    expect(Scenario.prototype.validate).called(scenario_invalide, '/', schema)
-      .to.throw();
-  });
-
-  it('expect valid "number" type of schema keys', function() {
-    let schema = { text: ['number'] };
-
-    let scenario_ok = { text: 999.9 };
-    expect(Scenario.prototype.validate).called(scenario_ok, '/', schema)
-      .to.not.throw();
-
-    let scenario_invalide = { text: "foobar" };
-    expect(Scenario.prototype.validate).called(scenario_invalide, '/', schema)
-      .to.throw();
-  });
-
 });
