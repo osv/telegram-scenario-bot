11d274fdf5af8e32fafbc6625510e7c0a9a80dce
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-07-09 23:21:23 +0300

    remove comment

--- src/scenario.js
+++ src/scenario.js
@@ -5,49 +5,6 @@ import _ from 'underscore';
 import Validator from './scenario-validator.js';
 import ScenarioWrapper from './scenario-wrapper.js';
 
-/*
-
-/start
- /setup
- .- /audio
- .   /ambient : set ambient audio level
- .   /music : set music level
- .- /video
- .   /resolution : large resolution select, use list:
-800x600 SVGA
-1024x600 WSVGA
-1024x768 XGA
-1152x864 XGA
-1280x720 WXGA
-1280x768 WXGA
-1280x800 WXGA
-1280x960 SXGA
-1280x1024 SXGA
-1360x768 HD
-1366x768 HD
-1400x1050 SXGA
-1440x900 WXGA
-1600x900 HD
-1600x1200 UXGA
-1680x1050 WSXGA
-1920x1080 FHD
-1920x1200 WUXGA
-2048x1152 QWXGA
-2560x1440 WQHD
-
- .    /1 item1
- .    /2 item2
- .    /3
- .    /more Show more /cancel
- .   /quality
- .    /1 high
- .    /2 low
- .    /3 medium
- /play
- /back
-*/
-
-
 /**
  * Create scenario container.
  * @param {object} [api] - api that may used by scenario using '<% ... %>' notation
@@ -103,29 +60,32 @@ function Scenario(api, scenario) {
 
 
   scenario_schema = {
-    name               : name,
-    typing             : boolean,
-    uploading_photo    : boolean,
-    recording_video    : boolean,
-    uploading_video    : boolean,
-    recording_audio    : boolean,
-    uploading_audio    : boolean,
-    uploading_document : boolean,
-    finding_location   : boolean,
+    name               : name,    // scenario name, unique
+    typing             : boolean, // action, typing
+    uploading_photo    : boolean, // action
+    recording_video    : boolean, // action
+    uploading_video    : boolean, // action
+    recording_audio    : boolean, // action
+    uploading_audio    : boolean, // action
+    uploading_document : boolean, // action
+    finding_location   : boolean, // action
 
-    reply              : string,
+    reply              : string, // reply message for this scenario
 
-    menu               : one_of_str_or_array,
+    menu               : one_of_str_or_array, // custom keyboard markup
 
-    ttl                : number,
+    ttl                : number, // time to live of session
 
-    before             : fun,
-    action             : fun,
+    before             : fun,   // called before  all, you may  use it
+                                // to   prepare   stash,   to   deduce
+                                // database operation
+
+    action             : fun,   // called before reply. Usually setter
     after              : fun,
 
-    goto               : string,
+    goto               : string, // next scenario.
 
-    commands           : command
+    commands           : command // hash, key - regexp, value - scenario object
   };
 
   this._validator = v;
@@ -254,152 +214,3 @@ Scenario.prototype = {
 };
 
 export default Scenario;
-
-// var scenario = {
-//   // Type of action to broadcast: Choose one, depending on what the user is about to receive
-//   typing: true,
-//   // upload_photo: true,
-//   // record_video: true,
-//   // upload_video: true,
-//   // record_audio: true,
-//   // upload_audio: true,
-//   // upload_document: true,
-//   // find_location: true,
-
-//   text: "Please select items:\n" +
-//     "/1 Hello worl\n" +
-//     "/2 Cancel",
-
-//   // commands
-//   answers: {
-//     commands: {
-//        ""
-//     }
-//     keyboard: [{
-//       "1 -> ^/1|^hell": {
-//         goto: "/",
-//         fun: async function() {
-//           console.log('selected item 1');
-//         },
-//       },
-//       "1 -> ^/2|^c": {
-//         goto: "/",
-//         fun: async function() {
-//           console.log('selected item 1');
-//         },
-
-//     }
-//     ],
-
-//     // answers format: "routeName -> regexp"
-//     },
-//     "2. "
-//   }
-
-// };
-
-// V = {
-//   boolean: function(key, value) {
-
-//   }
-// };
-
-// var scenario = {
-//   "Typing": true,
-//   "Before": "<% beforeRoot %>",
-//   "Commands": {
-//     "/start": {
-//       Scenario: {
-//         Commands: {
-//           "/audio": {
-//             "Text": "Audio settings, use /ambient or /music commands",
-//             "Menu": [
-//               "Ambient",
-//               "Music",
-//               ["Back", "Cancel"]
-//             ],
-//             "Scenario": {
-//               Menu: [
-//                 "0",
-//                 "20",
-//                 "50",
-//                 "75",
-//                 "100",
-//                 ["Back", "Cancel"]
-//               ],
-//               "/ambient | ambient": {
-//                 text: "Tell me, ambient level, number 0..100, current level is <% getAmbientLvl %>.",
-//                 "Scenario": {
-//                   "/back || back": {
-//                     "Goto": "<% _back %>"
-//                   },
-//                   "/cancel || cancel": {
-//                     "Goto": "/"
-//                   },
-//                   "default": {
-//                     "Action": "<% setAmbientMaybe %>",
-//                     "goto": "<% _back %>"
-//                   }
-//                 },
-//               },
-//               "/music || music" : {
-//                 Menu: [
-//                   "0",
-//                   "20",
-//                   "50",
-//                   "75",
-//                   "100",
-//                   ["Back", "Cancel"]
-//                 ],
-//                 text: "Tell me, music level, number 0..100, current level is <% getMusicLvl%>",
-//                 Scenario: {
-//                   "/back": {
-//                     "Goto": "<% _back %>"
-//                   },
-//                   "/cancel": {
-//                     "Goto": "/"
-//                   },
-//                   "default": {
-//                     Action: "<% setMusicMaybe %>",
-//                     "goto": "<% _back %>"
-//                   }
-//                 }
-//               }
-//             }
-//           },
-//           "/video": {
-
-//           },
-//           "cancel": {
-//           }
-//         }
-//       }
-//     },
-//     default: "<% defaultRoot %>"
-//   }
-// }
-
-// var minimal = {
-//   typing: V.boolean,
-//   upload_photo: V.boolean,
-//   record_video: V.boolean,
-//   upload_video: true,
-//   record_audio: true,
-//   upload_audio: true,
-//   upload_document: true,
-//   find_location: true,
-
-//   Scenario: {
-//     Text: "",
-//     Menu: [ // array of (string || array of string)
-
-//     ],
-//     Before: "<>",                  // run when enter to this scenario
-//     Action: "<>",                  //
-//     Goto: "/",
-//     Commands: {
-//       "command name": SCENARIO
-//     }
-//   }
-
-// },
